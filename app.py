656f3a6e8432673337adf62c96b4b764d0fc6558
Appar <32193913+appars@users.noreply.github.com>
2019-02-12 16:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to Simple Webapp Docker demo with OpenShift!"
+    return "Welcome to simple OpenShift Demo!!!!"
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return 'I am good, Thanks. How about you?'
 
 if __name__ == "__main__":
     app.run()
