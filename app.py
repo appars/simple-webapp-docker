a97bc4f3732b66cf785657656a34e08fa20d478c
Appar <apperu09@in.ibm.com>
2020-04-20 09:10:40 +0530

    app change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "CEDP: Welcome to simple OpenShift Demo!!!!"
+    return "GCDO: Welcome to simple OpenShift Demo!!!!"
 
 @app.route('/howareyou')
 def hello():
